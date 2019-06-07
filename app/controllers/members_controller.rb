35146e1415f06d85d37316133b3a9b1ac22efa8b
Leila Weiss <leila.weiss@powerhrg.com>
2019-06-07 11:02:35 -0400

    members

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,7 +1,47 @@
 class MembersController < ApplicationController
-  before_action :authenticate_member!
+  before_action :authenticate_member!, only: :index
   def index
     @members = Member.all
   end
 
+  def new
+    @member = Member.new
+  end
+
+  def show
+    @member = Member.find(params[:id])
+  end
+
+  def create
+    @member = Member.new(member_params)
+    if @member.save
+      redirect_to members_path(@member)
+    else
+      redirect_to new_member_path
+    end
+  end
+
+  def edit
+    @member = Member.find(params[:id])
+  end
+
+  def update
+    @member = Member.find(params[:id])
+    if @member.update(member_params)
+      redirect_to member_path
+    else
+      render "edit"
+    end
+  end
+
+  private
+
+  def member_params
+    params.require(:member).permit(
+      :first_name,
+      :last_name,
+      :email,
+      :encrypted_password
+    )
+  end
 end
